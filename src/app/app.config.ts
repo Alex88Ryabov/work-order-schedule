f8c144a75920735722d4e05cfe59cf35700f86c9
AlexRyabov <ryabovsasha88@gmail.com>
2026-06-16 08:38:10 +0300

    [naologic][technical-test] remove routing,

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 
-import { routes } from './app.routes';
 import { DotDateParserFormatter } from './core/utils/dot-date-parser-formatter';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     { provide: NgbDateParserFormatter, useClass: DotDateParserFormatter },
   ],
 };
